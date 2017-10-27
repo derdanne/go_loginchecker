5d68745284870ce53a0551bbcc81149d605eb6a7
Daniel Klockenkämper <dk@marketing-factory.de>
2017-10-27 17:58:34 +0200

    add example config

--- src/loginchecker/loginchecker.go
+++ src/loginchecker/loginchecker.go
@@ -6,17 +6,20 @@ import (
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
 	"log"
-	"fmt"
 	"strings"
 	"bufio"
 	"bytes"
+	"time"
 )
 
 type config struct {
 	Mailfrom string `yaml:"mailfrom"`
+	Mailfromname string `yaml:"mailfromname"`
 	Mailsubject string `yaml:"mailsubject"`
 	Mailtos []string `yaml:"mailtos"`
 	Ipaddresses []string `yaml:"ipaddresses"`
+	Rechecktime int64 `yaml:"rechecktime"`
+	Gracetime int64 `yaml:"gracetime"`
 }
 
 func (c *config) getConfig(configfile string) *config {
@@ -56,7 +59,7 @@ func getWho() string {
 	return string(whoOut)
 }
 
-func isAllowed(address string, allowedAddresses []string) bool {
+func isNotAllowed(address string, allowedAddresses []string) bool {
 	for _, allowedAddress := range allowedAddresses {
 		if allowedAddress == address {
 			return false
@@ -66,57 +69,47 @@ func isAllowed(address string, allowedAddresses []string) bool {
 }
 
 func hostname() string {
-	uname, lookErr := exec.LookPath("uname")
+	hostname, lookErr := exec.LookPath("hostname")
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
-	args := []string{"-n"}
+	args := []string{"-f"}
 
-	hostname, execErr := exec.Command(uname, args...).Output();
+	hostfqdn, execErr := exec.Command(hostname, args...).Output();
 	if execErr != nil {
 		panic(execErr)
 	}
 
-	return string(hostname)
+	return string(hostfqdn)
 }
 
 
 
-func sendMail(recipient string, mailfrom string, mailsubject string, mailbody string) {
+func sendMail(recipient string, mailfrom string, mailfromname string, mailsubject string, mailbody string) {
 
 	var sendmailBuffer bytes.Buffer
 
-	sendmailBuffer.WriteString("\nTo: ")
-	sendmailBuffer.WriteString(recipient)
-	sendmailBuffer.WriteString("\nSubject: ")
+	sendmailBuffer.WriteString("Subject: ")
 	sendmailBuffer.WriteString(mailsubject)
 	sendmailBuffer.WriteString("\nLoginchecker triggered an alert\n")
-	sendmailBuffer.WriteString("\n\n\n")
+	sendmailBuffer.WriteString("\n\n")
 	sendmailBuffer.WriteString(mailbody)
 	sendmailBuffer.WriteString("\n.")
-
-	fmt.Println(sendmailBuffer.String())
 
 	mail, lookErr := exec.LookPath("sendmail")
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
-	args := []string{"-t", "-f", mailfrom}
+	args := []string{"-t", "-f", mailfrom, "-F", mailfromname, recipient}
 
 	sendmail := exec.Command(mail, args...);
 	sendmail.Stdin = strings.NewReader(sendmailBuffer.String())
-	var out bytes.Buffer
-	sendmail.Stdout = &out
 	execErr := sendmail.Run()
-
 	if execErr != nil {
 		log.Fatal(execErr)
 	}
-	fmt.Printf("in all caps: %q\n", out.String())
-
-	fmt.Println(recipient, mailfrom, mailsubject, sendmailBuffer.String())
 }
 
 
@@ -134,22 +127,51 @@ func main () {
 
 	config.getConfig(configfile)
 
+	recheckTime := time.Duration(config.Rechecktime) * time.Second
+	graceTime := time.Duration(config.Gracetime) * time.Second
 	alertMailtos := config.Mailtos
 	allowedAddresses := config.Ipaddresses
 	mailFrom := config.Mailfrom
+	mailFromName := config.Mailfromname
 	mailSubject := config.Mailsubject + " @ " + hostname()
-	fmt.Println("Allowed addresses: ", allowedAddresses)
 
-	who := getWho()
-	scanner := bufio.NewScanner(strings.NewReader(who))
-	for scanner.Scan() {
-		line = scanner.Text()
-		whoIsLoggedIn := strings.Fields(line)
-		if isAllowed(strings.Trim(whoIsLoggedIn[6], "(,)"), allowedAddresses) {
-			mailBody := whoIsLoggedIn[0] + " is not allowed from " + strings.Trim(whoIsLoggedIn[6], "(,)")
-			for _, alertMailto := range alertMailtos {
-				sendMail(alertMailto, mailFrom, mailSubject, mailBody)
+	println(mailFrom, mailFromName, mailSubject)
+
+	logged := make(map[string]time.Time)
+
+	for {
+		who := getWho()
+		scanner := bufio.NewScanner(strings.NewReader(who))
+		for scanner.Scan() {
+			line = scanner.Text()
+			whoIsLoggedIn := strings.Fields(line)
+			username := whoIsLoggedIn[0]
+			address := strings.Trim(whoIsLoggedIn[6], "(,)")
+                        uniqe_user := username + address
+
+			if isNotAllowed(address, allowedAddresses) {
+				mailBody := username + " is not allowed from " + address
+				sendmail := false
+
+				timeDetected := time.Now()
+				if timeLogged, ok := logged[uniqe_user]; ok {
+					if (timeDetected.Sub(timeLogged) > graceTime) {
+						logged[uniqe_user] = timeDetected
+						sendmail = true
+					}
+
+				} else {
+					logged[uniqe_user] = timeDetected
+					sendmail = true
+				}
+
+				if sendmail == true {
+					for _, alertMailto := range alertMailtos {
+						sendMail(alertMailto, mailFrom, mailFromName, mailSubject, mailBody)
+					}
+				}
 			}
 		}
+		time.Sleep(recheckTime)
 	}
 }
